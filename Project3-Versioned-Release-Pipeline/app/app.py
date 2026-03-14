6069a91538f5f346cb9c201e5d439ff95fa01c56
Abdullah Butt <abdullahbuttpkr15@gmail.com>
2026-03-14 14:26:00 +0500

    Started the next day after failure

--- Project3-Versioned-Release-Pipeline/app/app.py
+++ Project3-Versioned-Release-Pipeline/app/app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Versioned CI/CD Pipeline Running!!!"
+    return "Versioned CI/CD Pipeline Running 🚀"
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
