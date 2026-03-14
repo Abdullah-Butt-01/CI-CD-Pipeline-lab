e59a2b3044dfce37d880fd2ebc807303157836e7
Abdullah Butt <abdullahbuttpkr15@gmail.com>
2026-03-14 15:09:04 +0500

    Updated app for another tag

--- Project3-Versioned-Release-Pipeline/app/app.py
+++ Project3-Versioned-Release-Pipeline/app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Versioned CI/CD Pipeline Running 🚀"
+    return "Versioned CI/CD Pipeline Running 🚀 tag 1.1, updated"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
