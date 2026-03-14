c0339f43acccb6e2b42508337b265c9d32995c4f
Abdullah Butt <abdullahbuttpkr15@gmail.com>
2026-03-14 15:31:24 +0500

    Updating app for v1.2 to test paths in workflow file okay or not

--- Project3-Versioned-Release-Pipeline/app/app.py
+++ Project3-Versioned-Release-Pipeline/app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Versioned CI/CD Pipeline Running 🚀 tag 1.1, updated"
+    return "Versioned CI/CD Pipeline Running 🚀 tag 1.2, Checking if paths in the workflow dont not cause problem again"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
